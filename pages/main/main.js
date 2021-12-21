7b610bb3a81674658f4d4663ba60ea8659a7bca0
annkarinacaro <anax1410@stud.kea.dk>
2021-12-21 03:19:03 +0100

    create/delete/getAll

---
+++ pages/main/main.js
@@ -0,0 +1,83 @@
+export default () => {
+    const main = document.querySelector(".content");
+    fetch("./pages/main/main.html")
+        .then((response) => response.text())
+        .then((html) => {
+            main.innerHTML = html;
+            getAllCandidates();
+        });
+};
+
+const backendURL = "http://localhost:8080";
+
+function getAllCandidates() {
+    fetch(backendURL + "/candidates")
+        .then((response) => response.json())
+        .then((candidates) => {
+            candidates.forEach((element) => console.log(element));
+            /*console.log(candidates[0].id);
+    console.log(candidates[0].name);
+    console.log(candidates[0].partyId.partyId);*/
+
+            const tableElement = document.querySelector(
+                "table.candidate-table"
+            );
+            tableElement.innerHTML = `
+    <tr>
+    <th>Name</th>
+    <th>Party id</th>
+    <th>Delete</th>
+    </tr>
+    `;
+
+            candidates.forEach((element) => {
+                const trElement = document.createElement("tr");
+                trElement.dataset.id = element.id;
+                tableElement.appendChild(trElement);
+
+                const nameTdElement = document.createElement("td");
+                nameTdElement.innerHTML = element.name;
+                trElement.appendChild(nameTdElement);
+
+                const partyIdTdElement = document.createElement("td");
+                partyIdTdElement.innerHTML = element.partyId.partyId;
+                trElement.appendChild(partyIdTdElement);
+
+                const deleteTdElement = document.createElement("td");
+                trElement.appendChild(deleteTdElement);
+
+                const deleteButtonElement = document.createElement("button");
+                deleteButtonElement.innerHTML = "delete";
+                deleteButtonElement.classList.add("cta");
+                deleteButtonElement.addEventListener("click", () => {
+                    fetch(backendURL + "/candidate/" + element.id, {
+                        method: "DELETE",
+                    })
+                        .then((response) => response.text())
+                        .then(() => {
+                            tableElement.removeChild(trElement);
+                        });
+                });
+                deleteTdElement.appendChild(deleteButtonElement);
+            });
+
+            const createBtn = document.querySelector("button");
+
+            createBtn.addEventListener("click", () => {
+                const name1 = document.querySelector(".name").value;
+                const partyId = document.querySelector(".partyId").value;
+
+                fetch("http://localhost:8080/candidate", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ name: name1, party_id: partyId }),
+                })
+                    .then((response) => response.text())
+                    .then((candidate) => {
+                        console.log(inputName);
+                    });
+            });
+        });
+}
---
+++ router.js
@@ -0,0 +1,12 @@
+import renderMain from "./pages/main/main.js";
+
+const router = new Navigo("/", { hash: true });
+
+router
+.on({
+"/": () =>{
+    renderMain();
+    router.updatePageLinks();
+},
+})
+.resolve();
